65ada877f5aac31f022cc5172199c7c843f25116
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-05 14:42:27 -0500

    Se añadio la clase PilaIguales

---
+++ PilasIguales/PilaIguales.java
@@ -0,0 +1,13 @@
+package PilasIguales;
+
+public class PilaIguales {
+    public static void main(String[]args) {
+		int[] pila1 = {1,1,1,2,3};
+		int[] pila2= {2,3,4};
+		int[] pila3 = {1,4,1,1};
+		System.out.println("La cantidad en que las 3 pilas son iguales es: "+equalStacks(pila1,pila2,pila3));
+	}
+    public static int equalStacks(int[] a,int[] b,int[] c) {
+        return 0;
+    }
+}
